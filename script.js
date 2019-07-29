92c7f575438b9e89495b309f1d1d2e120ae217f2
BushraSheeraz <49517146+BushraSheeraz@users.noreply.github.com>
2019-07-29 14:22:24 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+function showOne() {
+    let rs = document.querySelector("#inp").value += 1;
+}
+function showTwo() {
+    let rs = document.querySelector("#inp").value += 2;
+} 
+function showThree() {
+    let rs = document.querySelector("#inp").value += 3;
+}
+function showFour() {
+    let rs = document.querySelector("#inp").value += 4;
+} 
+function showFive() {
+    let rs = document.querySelector("#inp").value += 5;
+} 
+function showSix() {
+    let rs = document.querySelector("#inp").value += 6;
+} 
+function showSeven() {
+    let rs = document.querySelector("#inp").value += 7;
+}
+function showEight() {
+    let rs = document.querySelector("#inp").value += 8;
+}
+function showNine() {
+    let rs = document.querySelector("#inp").value += 9;
+}
+function showZero() {
+    let rs = document.querySelector("#inp").value += 0;
+}
+
+function showAdd() {
+    document.querySelector("#inp").value += " + ";
+}
+function showSub() {
+    document.querySelector("#inp").value += " - ";
+}
+function showMultiply() {
+    document.querySelector("#inp").value += " * ";
+}
+function showDivide() {
+    document.querySelector("#inp").value += " / ";
+}
+function showDecimal(){
+    document.querySelector("#inp").value += " . ";
+}
+function clearScreen(){
+    document.querySelector("#inp").value = "  ";
+    document.querySelector("#inp2").value= "  ";
+}
+// function removeItem(){
+//     document.querySelector("#inp1").value -= document.querySelector("#inp1").value;
+// }
+function result(){
+    document.querySelector("#inp2").value += " = " + parseInt(eval(document.querySelector("#inp").value));
+}
